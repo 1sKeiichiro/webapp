daaf6be67c2cd27cc4571e17b82dc187125968e2
Kyamada <yamada39keichiro+github@gmail.com>
2025-03-10 23:36:42 +0900

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,21 @@
+// src/lib/db.ts
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 import path from 'path';
 import fs from 'fs';
 
-const DB_PATH = path.join(process.cwd(), 'data', 'data.sqlite');
+// Lambda環境の一時書き込み可能フォルダを利用
+const tmpDbPath = '/tmp/data.sqlite';
+const originalDbPath = path.join(process.cwd(), 'data', 'data.sqlite');
 
-if (!fs.existsSync(DB_PATH)) {
-  throw new Error(`Database file not found at path: ${DB_PATH}`);
+// Lambda関数内で毎回実行されるため、一度だけコピーする
+if (!fs.existsSync(tmpDbPath)) {
+  console.log('📂 DB file copying to tmp...');
+  fs.copyFileSync(originalDbPath, tmpDbPath);
 }
 
 const dbPromise = open({
-  filename: DB_PATH,
+  filename: tmpDbPath,
   driver: sqlite3.Database,
 });
 
